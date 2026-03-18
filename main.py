f04cf347ab00e009d56cc817f0b3945d73f1da53
JPNat <jpnatividade2016@gmail.com>
2026-03-17 21:44:21 -0300

    Melhoria de Deploy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from routes.categorizar_router import categorizar_router
 
 app = FastAPI()
@@ -17,6 +20,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def read_index():
+    caminho_html = os.path.join(os.getcwd(), "FrontEnd", "index.html")
+    if os.path.exists(caminho_html):
+        return FileResponse(caminho_html)
+    return {"erro": "Arquivo index.html não encontrado no servidor", "path": caminho_html}
 
 @app.get('/developers')
 def read_developers() -> dict[str, list[str]]:
